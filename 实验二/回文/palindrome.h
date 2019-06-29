74792c728e1d1e14466f23718b2b30eed576ca5d
Rui <42614394+MaIUR@users.noreply.github.com>
2019-06-29 10:45:38 +0800

    Create palindrome.h

---
+++ 实验二/回文/palindrome.h
@@ -0,0 +1,20 @@
+/************************************************* 
+Author: MaIUR
+Date:2017-11-15
+**************************************************/ 
+
+/*palindromr类
+数据成员：number n1 n2 n3 n4 n5
+成员函数：division modulus compare */
+
+using namespace std;
+
+class Palindrome
+{
+	int number;
+	int n1, n2, n3, n4, n5;
+public:
+	Palindrome(int);
+	void division();
+	void compare();
+};
