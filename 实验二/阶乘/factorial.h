07559c3e6e4f19ea443c4cbc298682bdc636a302
Rui <42614394+MaIUR@users.noreply.github.com>
2019-06-29 10:43:35 +0800

    Create factorial.h

---
+++ 实验二/阶乘/factorial.h
@@ -0,0 +1,12 @@
+/************************************************* 
+Author: MaIUR
+Date:2017-11-16
+**************************************************/ 
+
+class Factorial
+{
+public:
+	double factorial(int);
+	void getE();
+	void getEx();
+};
